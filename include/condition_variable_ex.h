a27c0288b0873520cb5bec55cfc68f4a7f14fad9
gerardcarbo <gcarbo@miraiblau.com>
2023-01-09 13:08:19 +0100

    Tests Added

--- include/condition_variable_ex.h
+++ include/condition_variable_ex.h
@@ -1,4 +1,5 @@
 #include <condition_variable>
+#include <map>
 
 using namespace std;
 
@@ -6,10 +7,23 @@ namespace gecaib _GLIBCXX_VISIBILITY(default)
 {
   enum class cv_status_ex
   {
-    signaled,
     timeout,
+    signaled,
     predicate
   };
+
+  std::ostream& operator<<(std::ostream& out, const cv_status_ex value){
+    static std::map<cv_status_ex, std::string> strings;
+    if (strings.size() == 0){
+#define INSERT_ELEMENT(p) strings[p] = #p
+        INSERT_ELEMENT(cv_status_ex::timeout);     
+        INSERT_ELEMENT(cv_status_ex::signaled);     
+        INSERT_ELEMENT(cv_status_ex::predicate);             
+#undef INSERT_ELEMENT
+    }   
+
+    return out << strings[value];
+}
 
   /// condition_variable
   class condition_variable_ex : public condition_variable
@@ -23,33 +37,36 @@ namespace gecaib _GLIBCXX_VISIBILITY(default)
 
     template <typename _Rep, typename _Period, typename _Predicate>
     cv_status_ex
-    wait_for(unique_lock<mutex> &__lock,
+    wait_for_ex(unique_lock<mutex> &__lock,
              const chrono::duration<_Rep, _Period> &__rtime,
              _Predicate __p)
     {
       using __dur = typename steady_clock::duration;
-      return wait_until(__lock,
+      return wait_until_ex(__lock,
                         steady_clock::now() +
-                            chrono::__detail::ceil<__dur>(__rtime));
+                            chrono::__detail::ceil<__dur>(__rtime), __p);
     }
 
     template <typename _Clock, typename _Duration, typename _Predicate>
     cv_status_ex
-    wait_until(unique_lock<mutex> &__lock,
+    wait_until_ex(unique_lock<mutex> &__lock,
                const chrono::time_point<_Clock, _Duration> &__atime,
                _Predicate __p)
     {
       while (!__p())
       {
-        const __clock_t::time_point __s_entry = __clock_t::now();
-        const auto __s_atime = __s_entry +
-                               chrono::__detail::ceil<_Duration>(chrono::milliseconds(100));
+        const auto __s_atime = __clock_t::now() + chrono::milliseconds(10);
         auto status = wait_until(__lock, __atime);
+        cout << "wait done: " << status << " pred: " << __p() << endl;
+        if(__p())
+        {
+          return cv_status_ex::predicate;
+        }
         if (status == cv_status_ex::signaled)
         {
           return cv_status_ex::signaled;
         }
-        if (status == cv_status_ex::timeout && __s_atime >= __atime)
+        if (__clock_t::now() >= __atime)
         {
           return cv_status_ex::timeout;
         }
--- main.cpp
+++ main.cpp
@@ -4,52 +4,76 @@
 #include <mutex>
 #include <condition_variable_ex.h>
 
+using namespace gecaib;
+
 std::mutex m;
-gecaib::condition_variable_ex cv;
-std::string my_data;
+condition_variable_ex cv;
+std::mutex m_done;
+condition_variable_ex cv_done;
 bool ready = false;
 bool processed = false;
+cv_status_ex finalStatus;
 
-void worker_thread()
+void worker_thread(int timeout)
 {
     // Wait until main() sends data
     std::unique_lock lk(m);
-    auto status = cv.wait_for(lk, chrono::microseconds(1000), []
-            { return ready; });
+    finalStatus = cv.wait_for_ex(lk, chrono::milliseconds(timeout), []{ return ready; });
 
     // after the wait, we own the lock.
-    std::cout << "Worker thread is processing data\n";
-    my_data += " after processing with status " + std::to_string((int)status);
+    std::cerr << "Wait finished: " << finalStatus << "\n";
 
     // Send data back to main()
     processed = true;
-    std::cout << "Worker thread signals data processing completed\n";
 
     // Manual unlocking is done before notifying, to avoid waking up
     // the waiting thread only to block again (see notify_one for details)
     lk.unlock();
-    cv.notify_one();
+    cv_done.notify_one();
 }
 
-int main(int, char **)
+void signaler_thread(int timeout)
 {
-    std::thread worker(worker_thread);
-    my_data = "Example data";
-    // send data to the worker thread
-    {
-        std::lock_guard lk(m);
-        ready = true;
-        std::cout << "main() signals data ready for processing\n";
-    }
-    cv.notify_one();
+    this_thread::sleep_for(chrono::milliseconds(timeout));
+    cv.notify_all();
+    std::cout << "signaled\n";
+}
+
+void predicater_thread(int timeout)
+{
+    this_thread::sleep_for(chrono::milliseconds(timeout));
+    std::lock_guard lk(m);
+    ready = true;
+    std::cout << "predicated\n";
+}
+
+int main(int n, char **args)
+{
+    cv_status_ex desiredStatus = (cv_status_ex)atoi(args[1]);
+    int thread_timeout = atoi(args[2]);
+    int signal_timeout = atoi(args[3]);
+    int predicate_timeout = atoi(args[4]);
+
+    cout << "thread_timeout: " << thread_timeout << " signal_timeout: " << signal_timeout << " predicate_timeout: " << predicate_timeout << "\n";
+
+    std::thread worker(worker_thread, thread_timeout);
+    std::thread signaler(signaler_thread, signal_timeout);
+    std::thread predicater(predicater_thread, predicate_timeout);
 
     // wait for the worker
     {
-        std::unique_lock lk(m);
-        cv.wait(lk, []
+        std::unique_lock lk(m_done);
+        cv_done.wait(lk, []
                 { return processed; });
     }
-    std::cout << "Back in main(), data = " << my_data << '\n';
-
+    
     worker.join();
+    signaler.join();
+    predicater.join();
+
+    if(desiredStatus == finalStatus){
+        return 0;
+    }
+    cout << "Failed: it should be " << desiredStatus << " and is " << finalStatus << "\n";
+    return 1;
 }
